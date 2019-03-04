e641478db3242f66ee639199dfa83a1b0ef455c3
obiukh19 <oleg.obiukh@gmail.com>
2019-03-04 19:07:24 +0200

    Deploy 1

---
+++ build/precache-manifest.256c5ba1debf9dd6ab6130216bfad487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-data-table/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a778ccc251a67742a8d",
+    "url": "/react-data-table/build/static/js/runtime~main.7a778ccc.js"
+  },
+  {
+    "revision": "658cee9e2ef34bf12165",
+    "url": "/react-data-table/build/static/js/main.658cee9e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-data-table/build/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "658cee9e2ef34bf12165",
+    "url": "/react-data-table/build/static/css/main.39dd1529.chunk.css"
+  },
+  {
+    "revision": "3abd324438ac1d9569cccf77961eadd1",
+    "url": "/react-data-table/build/index.html"
+  }
+];
