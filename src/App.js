258673c3a2e47888d08426b2072db27d68b1480c
obiukh19 <oleg.obiukh@gmail.com>
2019-03-04 19:15:18 +0200

    todo list code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+const url =
+  "https://mate-academy.github.io/phone-catalogue-static/phones/phones.json";
 
 class App extends Component {
+  state = {
+    phones: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      phones: data
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { phones } = this.state;
+
+    return phones ? this.renderData(phones) : this.renderLoading();
+  }
+
+  renderData(phones) {
+    // console.log(this.state.phones);
+    if (phones && phones.length) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>age</th>
+              <th>snippet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {phones.map(item => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.snippet}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    } else {
+      return <div>No items found</div>;
+    }
+  }
+  renderLoading() {
+    return <div>Loading...</div>;
   }
 }
 
