34c2912478135fd3958ae3407f49e367e0ea36d0
obiukh19 <oleg.obiukh@gmail.com>
2019-03-25 01:24:33 +0200

    Updates

---
+++ precache-manifest.24684c16781f202c60a6d286485d3179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d89c2a62611cc85335",
+    "url": "/react-data-table/static/js/runtime~main.39398cdc.js"
+  },
+  {
+    "revision": "718fc69b4f21b6ad066a",
+    "url": "/react-data-table/static/js/main.2591f47c.chunk.js"
+  },
+  {
+    "revision": "895926865f6869620105",
+    "url": "/react-data-table/static/js/2.904a862c.chunk.js"
+  },
+  {
+    "revision": "718fc69b4f21b6ad066a",
+    "url": "/react-data-table/static/css/main.03ff9b0b.chunk.css"
+  },
+  {
+    "revision": "895926865f6869620105",
+    "url": "/react-data-table/static/css/2.01e5f67d.chunk.css"
+  },
+  {
+    "revision": "3d8d3d7ed828e7886e44997c15036831",
+    "url": "/react-data-table/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-data-table/precache-manifest.59adf837fc78c4840fce28ed942050da.js"
+  "/react-data-table/precache-manifest.24684c16781f202c60a6d286485d3179.js"
 );
 
 workbox.clientsClaim();
